4f0084556a997b4967d1dc1ee090413191a1bceb
Dimeji Jason <hexadigitztech@gmail.com>
2020-03-02 13:07:53 +0100

    reviews

---
+++ signUp.php
@@ -0,0 +1,11 @@
+<html>
+    
+
+<body>
+
+Welcome <?php echo $_POST["secondN"]; ?><br>
+Your sign up was successful. <br> 
+Check your mail <?php echo $_POST["email"]; ?> to activate your account.
+
+</body>
+</html
